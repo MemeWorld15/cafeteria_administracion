9319db5fa28257ecad44ad09a6cfdf2c2516fb5d
José Manuel Maldonado Monzón <josemanuelmaldonadomonzon5@gmail.com>
2025-08-05 11:53:37 -0600

    conexion back

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from psycopg2.extras import RealDictCursor
 
 def get_db_connection():
     return psycopg2.connect(
-        host="https://e548793ec54c.ngrok-free.app",  # Host
+        host="localhost",  # Host
         user="postgres",   # Usuario
         password="2612",   # Contraseña
         database="cafe_admin",  # Base de datos
