cb2d1f28f0e0fb26d76fe26ab6b79966a80e6dbb
José Manuel Maldonado Monzón <josemanuelmaldonadomonzon5@gmail.com>
2025-08-05 14:38:02 -0600

    local

--- backend/config.py
+++ backend/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BACKEND_URL = os.getenv("BACKEND_URL", " https://6c097db88ac9.ngrok-free.app")
+BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8000")
 FRONTEND_URL = os.getenv("FRONTEND_URL", "https://cafeteria-administracion.vercel.app")
